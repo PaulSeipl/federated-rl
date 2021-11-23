ae30a67269096669091b4df4e8b3c314707d98d6
Paul Seipl <paul.seipl@auteon.de>
2021-11-23 18:09:05 +0100

    run multiple agents with different envs and average state dicts after an interval

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import mean
 from src import rooms, a2c
 import matplotlib.pyplot as plot
 from os import walk
@@ -20,7 +21,7 @@ def episode(env, agent, nr_episode=0):
         state = next_state
         discounted_return += reward * (discount_factor ** time_step)
         time_step += 1
-    print(nr_episode, ":", discounted_return)
+    print(agent.name, ", ", nr_episode, ":", discounted_return)
     return discounted_return
 
 
@@ -54,14 +55,29 @@ def get_parameters(env, alpha=0.001, gamma=0.99):
         # Hyperparameters
         "alpha": alpha,  # learning rate
         "gamma": gamma,  # discount factor
+        # "id"
+        "name": f"Progress: {env.movie_filename.replace('.mp4', '')}",
     }
+
+
+def initialize_main_agent(max_steps):
+    env = rooms.load_env(
+        "layouts/9_9_4/test/rooms_9_9_4_t0.txt",
+        "movies/rooms_9_9_4_t0.mp4",
+        max_steps,
+    )
+    agent = a2c.A2CLearner(get_parameters(env, alpha=0.001, gamma=0.99))
+    # agent.a2c_net.train(False)
+    return env, agent
 
 
 def main():
     # Setup file names
-    training_episodes = 1
+    test_episodes = 1
+    working_intervals = 1
     max_steps = 100
     rooms_dir = "9_9_4"
+    test_env, main_agent = initialize_main_agent(max_steps)
 
     room_files = get_room_files(rooms_dir)
 
@@ -76,34 +92,63 @@ def main():
     ]
 
     # params
-    params = get_parameters(envs[0], alpha=0.001, gamma=0.99)
-    print(f"params: {params}")
+    print(f"params: {get_parameters(envs[0], alpha=0.001, gamma=0.99)}")
 
     # multiple agents
-    env_agent_pairs = [(env, a2c.A2CLearner(params)) for env in envs]
+    worker_agents = [
+        a2c.A2CLearner(get_parameters(env, alpha=0.001, gamma=0.99)) for env in envs
+    ]
+    # run x "training_episodes" for every agent "working_intervals" times
+    for _ in range(working_intervals):
+        worker_state_dicts = []
+        for (env, worker) in zip(envs, worker_agents):
+            # initialize each agent with state_dict from main agent
+            worker.load_state_dict(main_agent.get_state_dict_copy())
 
-    for (env, agent) in env_agent_pairs:
-        returns = [episode(env, agent, i) for i in range(training_episodes)]
-        x_data = range(training_episodes)
-        y_data = returns
-        create_plot(
-            f"Progress: {env.movie_filename.replace('.mp4', '')}",
-            x_data,
-            "episode",
-            y_data,
-            "discounted return",
-        )
-        env.save_video()
+            # run {training_episodes} episodes for each agent
+            [episode(env, worker, i) for i in range(test_episodes)]
 
-    # Agent setup
-    # main_agent = a2c.A2CLearner(params)
-    # returns = [episode(env, main_agent, i) for i in range(training_episodes)]
+            # safe worker state_dict
+            worker_state_dicts.append(worker.get_state_dict_copy())
 
+        # calculate mean of the state_dicts
+        mean_state_dict = {}
+        for key in main_agent.get_state_dict().keys():
+            mean_state_dict[key] = sum(
+                [worker_state_dict[key] for worker_state_dict in worker_state_dicts]
+            ) / len(worker_state_dicts)
+
+        # set main_agent state dict
+        main_agent.load_state_dict(mean_state_dict)
+
+    test_episodes = 100
+    # run test with main_agent
+    # main_agent.a2c_net.eval()
+    returns = [episode(test_env, main_agent, i) for i in range(test_episodes)]
+
+    x_data = range(test_episodes)
+    y_data = returns
+
+    create_plot(
+        f"Progress: {test_env.movie_filename.replace('.mp4', '')}",
+        x_data,
+        "episode",
+        y_data,
+        "discounted return",
+    )
+
+    # test_env.save_video()
+    main_agent.save_net("main_agent")
+    # returns = [episode(env, agent, i) for i in range(training_episodes)]
     # x_data = range(training_episodes)
     # y_data = returns
-
-    # create_plot("Progress", x_data, "episode", y_data, "discounted return")
-
+    # create_plot(
+    #     f"Progress: {env.movie_filename.replace('.mp4', '')}",
+    #     x_data,
+    #     "episode",
+    #     y_data,
+    #     "discounted return",
+    # )
     # env.save_video()
 
 
